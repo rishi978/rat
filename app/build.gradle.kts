0dab7754bdb09d916e8d92b2724cb293bc146566
aniket <aniketsachan111@gmail.com>
2026-08-10 16:03:18 +0500

    first build

---
+++ app/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.pentest.rat"
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "com.pentest.rat"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        buildConfigField("String", "C2_URL", "\"ws://10.74.211.102:4444\"") // ← APNA IP DAALO
+    }
+    buildFeatures { buildConfig = true }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+}
